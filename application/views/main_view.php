76febb9a46e2c5b0c62eb459e55fb14591b05204
ctd25 <cdemundo@gmail.com>
2014-11-07 14:04:21 -0500

    load all programs on initial load

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -40,7 +40,7 @@
 	              </ul>
 	            </li>
 	          </ul>
-	          <ul class="nav navbar-nav navbar-right">
+	          <ul class="nav navbar-nav navbar-right"
 	            <li><a href="<?php echo base_url()?>main/logout/">Logout</a></li>
 	          </ul>
 	        </div><!--/.nav-collapse -->
@@ -73,17 +73,16 @@
 		            </div>
 		            <!-- /sidebar -->
 		          
-		            <!-- main right col -->
+		            <!-- main content -->
 		            <div class="column col-sm-10 col-xs-11" id="main">
-		                	                
-		              
+	
 		                <div class="padding">
 		                    <div class="full col-sm-9">
 		                      
 		                        <!-- content -->                      
 		                      	<div class="row">
 		                      		<!-- Header in top of content section -->
-		                        	<div class = "col-sm-12">
+		                        	<div class = "col-sm-12 hidden-xs">
 		                        		<div class="panel panel-default">
 			                                <div class="panel-body">
 			                                	<p class="lead no-bottom">Residency Locator</p>
@@ -91,12 +90,21 @@
 		                              	</div>
 		                        	</div>
 
+		                        	<!-- FIXES BUG WHERE NAVBAR OVERLAPS ON SMALLER BROWSERS, USES BIG-TOP-SPACER CLASS -->
+		                        	<div class = "col-sm-12 visible-xs">
+		                        		<div class="panel panel-default big-top-spacer">
+			                                <div class="panel-body">
+			                                	<p class="lead no-bottom">Residency Locator</p>
+			                                </div>
+		                              	</div>
+		                        	</div>
+
 		                        	<!-- US MAP DIV -->
-		                        	<!-- HIDE MAP ON PHONES -->
+		                        	<!-- MAP FOR SCREENS BIGGER THAN XS -->
 		                        	<div class = "col-sm-12 hidden-xs">
 		                        		<div class="panel panel-default">
-			                                <div class="panel-body" id = "map" width="768">
-
+			                                <div class="panel-body text-center" id = "map" style="width: 768px; height: 500px;">
+			                                	<!-- map goes here -->
 			                                </div>
 		                              	</div>
 		                        	</div>
@@ -104,16 +112,90 @@
 		                        	<!--VISIBLE ON PHONES -->
 		                        	<div class = "col-sm-12 visible-xs">
 		                        		<div class="panel panel-default">
-			                                <div class="panel-body" id = "map" width="768">
-			                                	<p> Interactive map is not available on small screen sizes.  Please scroll through all programs. </p>
+			                                <div class="panel-body" id = "map">
+			                                	<select class="form-control" id = "stateDropdown">
+													<option value="AL">Alabama</option>
+													<option value="AK">Alaska</option>
+													<option value="AZ">Arizona</option>
+													<option value="AR">Arkansas</option>
+													<option value="CA">California</option>
+													<option value="CO">Colorado</option>
+													<option value="CT">Connecticut</option>
+													<option value="DE">Delaware</option>
+													<option value="DC">District Of Columbia</option>
+													<option value="FL">Florida</option>
+													<option value="GA">Georgia</option>
+													<option value="HI">Hawaii</option>
+													<option value="ID">Idaho</option>
+													<option value="IL">Illinois</option>
+													<option value="IN">Indiana</option>
+													<option value="IA">Iowa</option>
+													<option value="KS">Kansas</option>
+													<option value="KY">Kentucky</option>
+													<option value="LA">Louisiana</option>
+													<option value="ME">Maine</option>
+													<option value="MD">Maryland</option>
+													<option value="MA">Massachusetts</option>
+													<option value="MI">Michigan</option>
+													<option value="MN">Minnesota</option>
+													<option value="MS">Mississippi</option>
+													<option value="MO">Missouri</option>
+													<option value="MT">Montana</option>
+													<option value="NE">Nebraska</option>
+													<option value="NV">Nevada</option>
+													<option value="NH">New Hampshire</option>
+													<option value="NJ">New Jersey</option>
+													<option value="NM">New Mexico</option>
+													<option value="NY">New York</option>
+													<option value="NC">North Carolina</option>
+													<option value="ND">North Dakota</option>
+													<option value="OH">Ohio</option>
+													<option value="OK">Oklahoma</option>
+													<option value="OR">Oregon</option>
+													<option value="PA">Pennsylvania</option>
+													<option value="RI">Rhode Island</option>
+													<option value="SC">South Carolina</option>
+													<option value="SD">South Dakota</option>
+													<option value="TN">Tennessee</option>
+													<option value="TX">Texas</option>
+													<option value="UT">Utah</option>
+													<option value="VT">Vermont</option>
+													<option value="VA">Virginia</option>
+													<option value="WA">Washington</option>
+													<option value="WV">West Virginia</option>
+													<option value="WI">Wisconsin</option>
+													<option value="WY">Wyoming</option>
+												</select>
 			                                </div>
 		                              	</div>
 		                        	</div>
 
+		                        	<!-- Residency Program content -->
+		                        	<div class = "col-sm-12 col-sm-offset-5">
+		                        		<p class="lead"> Residency Programs</p>
+		                        	</div>
+		                        	
+
 		                        	<!-- RESIDENCY PROGRAM INFO GOES HERE -->
-		                        	<div class = "col-sm-12">
+		                        	<div class = "col-sm-12 equalheight">
 		                        		<div id = "residencyinfo">
-		                        			<!-- ajax goes here -->
+		                        			<?php
+		                        			if(isset($residencyProgram))
+											{
+												foreach($residencyProgram as $program):?>
+												<!-- Uses col-sm so that on mobile devices, these should stack so the user can scroll down.  on bigger screens - display 3xn table -->
+												
+												<div class="col-sm-4">
+													<div class = "panel panel-default demo">
+														<div class="panel-body">
+															<a href='<?php echo site_url();?>residencyprogram/getProgram/<?php echo $program->program_name ?>' class="lead bottom-spacer"> <?php echo $program->program_name ?> </a>
+															<p class="top-spacer"> State: <?php echo $program->state ?> </p>
+															<p> Director: <?php echo $program->director ?> </p>
+														</div>
+													</div>
+												</div>
+
+												<?php endforeach; } ?>
 		                              	</div>
 		                        	</div>
 		                        
@@ -155,32 +237,6 @@
 		    </div>
 		</div>
 
-
-	<!--post modal-->
-	<div id="postModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
-	  <div class="modal-dialog">
-	  <div class="modal-content">
-	      <div class="modal-header">
-	          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-				Update Status
-	      </div>
-	      <div class="modal-body">
-	          <form class="form center-block">
-	            <div class="form-group">
-	              <textarea class="form-control input-lg" autofocus="" placeholder="What do you want to share?"></textarea>
-	            </div>
-	          </form>
-	      </div>
-	      <div class="modal-footer">
-	          <div>
-	          <button class="btn btn-primary btn-sm" data-dismiss="modal" aria-hidden="true">Post</button>
-	            <ul class="pull-left list-inline"><li><a href=""><i class="glyphicon glyphicon-upload"></i></a></li><li><a href=""><i class="glyphicon glyphicon-camera"></i></a></li><li><a href=""><i class="glyphicon glyphicon-map-marker"></i></a></li></ul>
-			  </div>	
-	      </div>
-	  </div>
-	  </div>
-	</div>
-
 	<!--CORE JS BOOTSTRAP AND JQUERY -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>	
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
@@ -210,22 +266,40 @@
     		$('#map').usmap({
     			showLabels: true,
     				click: function(event, data) {
-    				var base_url = '<?php echo site_url();?>'; 
-					$.ajax({
-					  type: "POST",
-					  url: base_url + 'residencyprogram' + '/getStateInfo',
-					  dataType: "html",
-					  data: { 'state' : data.name },
-					  success: function(data) {
-							$('#residencyinfo').html(data);
-							}
-						})
+						getPrograms(data.name);
 					}
     		});
   		});
 	</script>
-		
 
+	<!-- SCRIPT FOR STATE DROPDOWN LIST.. SHOWS UP ON PHONES INSTEAD OF MAP -->
+	<script>
+		$(document).ready(function() {
+			$('#stateDropdown').on('change', function() {
+				console.log($("#stateDropdown").val());
+				var state = $("#stateDropdown").val(); 
+				getPrograms(state); 
+			});
+		});
+	</script>
+	
+	<!-- Jquery function, ajax to get residency program info - takes state abbreviation as input 
+		 ajax request to residencyprogram/getStateInfo                                      	-->
+	<script>
+		function getPrograms(state) {
+			var base_url = '<?php echo site_url();?>'; 
+
+			$.ajax({
+			  type: "POST",
+			  url: base_url + 'residencyprogram' + '/getStateInfo',
+			  dataType: "html",
+			  data: { 'state' : state },
+			  success: function(data) {
+					$('#residencyinfo').html(data);
+					}
+				})
+		}
+	</script>	
 	</body>
 </html>
 
