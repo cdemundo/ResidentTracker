197de0041f41798d5acdc35cd27197bd14e36d4e
ctd25 <cdemundo@gmail.com>
2014-11-07 14:05:11 -0500

    ajax html update

---
+++ application/views/resprogramsbystate_view.php
@@ -0,0 +1,22 @@
+<?php
+
+if(isset($residencyProgram))
+{
+
+	foreach($residencyProgram as $program):?>
+	<!-- Uses col-sm so that on mobile devices, these should stack so the user can scroll down.  on bigger screens - display 3xn table -->
+	
+	<div class="col-sm-4">
+		<div class = "panel panel-default">
+			<div class="panel-body">
+				<a href='<?php echo site_url();?>residencyprogram/getProgram/<?php echo $program->program_name ?>' class="lead bottom-spacer"> <?php echo $program->program_name ?> </a>
+				<p class="top-spacer"> State: <?php echo $program->state ?> </p>
+				<p> Director: <?php echo $program->director ?> </p>
+			</div>
+		</div>
+	</div>
+
+	<?php endforeach;
+} ?>
+
+
